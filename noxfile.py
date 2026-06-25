e0eb7977be7dc0946cb69899872c4910fa9ee311
blanky <aaishwarymishra@gmail.com>
2026-06-25 17:40:48 +0530

    improve yaml formatting and make tests run on recent versions

--- noxfile.py
+++ noxfile.py
@@ -15,7 +15,7 @@ def lint(session):
     session.run("black", "--check", "--diff", *args)
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.11", "3.12", "3.13", "3.14"])
 def tests(session):
     session.install(
         "torch==2.2.1",
